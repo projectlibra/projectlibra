b061d873706dd4f9b19ab718164430930e349dd8
Abdullah Talayhan <abdullahttj@gmail.com>
2020-01-06 15:28:59 +0300

    Edit Patients

--- libra-frontend/src/App.js
+++ libra-frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
+import editPatient from './pages/editPatient';
+
+// Components
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          {/* Insert Page routes here: */}
+          {/*<Route path="/" component={}/>*/}
+          <Route exact path="/editPatient" component={editPatient}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
