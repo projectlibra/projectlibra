7dd239c30047c08062f03f8d617e2000a4570c03
Mahmud Sami Aydın <mahmudsami97@gmail.com>
2020-01-06 16:05:39 +0300

    router added
    app.js route added

--- libra-frontend/src/App.js
+++ libra-frontend/src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import Login from './components/Login'
+import Forgot from './components/Forgot'
+import CreatePatientProfile from './components/CreatePatientProfile'
+import { BrowserRouter as Router,  Switch,  Route,  Link} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      
+    
+  	<Router>
+    	<Switch>
+    		<Route exact path="/login">
+				<Login />
+			</Route>
+			<Route exact path="/forgot">
+				<Forgot />
+			</Route>
+			<Route exact path="/createPatientProfile">
+				<CreatePatientProfile />
+			</Route>
+		</Switch>
+		
+    </Router>
     </div>
   );
 }
