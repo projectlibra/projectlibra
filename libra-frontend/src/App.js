f1e50244af9a6e8df9e301520da5c6063ee6b12d
NaisilaPuka <nicypp@gmail.com>
2020-01-06 21:13:48 +0300

    Add upload option only for VCF files

--- libra-frontend/src/App.js
+++ libra-frontend/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
 import BuildQuery from './components/BuildQuery';
+import VCFDrop from './components/VCFDrop';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
 function App() {
   
@@ -12,6 +14,17 @@ function App() {
           {/* Insert Page routes here: */}
           {/*<Route path="/" component={}/>*/}
           <Route exact path="/buildquery" component={BuildQuery}/>
+          <Route exact path="/vcfupload" component={VCFDrop}/>
+          <Button
+  variant="contained"
+  component="label"
+>
+  Upload File
+  <input
+    type="file"
+    style={{ display: "none" }}
+  />
+</Button>
         </Switch>
       </Router>
     </div>  
