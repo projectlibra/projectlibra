e5f7c6de581780ebe9d4022934fe6c785c57c21d
Mahmud Sami Aydın <mahmudsami97@gmail.com>
2020-01-07 13:27:16 +0300

    nationality selection adapted
    nationality selection adapted

--- libra-frontend/src/components/CreatePatientProfile.js
+++ libra-frontend/src/components/CreatePatientProfile.js
@@ -2,7 +2,11 @@ import React, {Component,useState, useContext} from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import TextfieldWithPublicity from "./TextfieldWithPublicity";
-
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import { CountryRegionData } from 'react-country-region-selector-material-ui-new';
 	
 function CreatePatientProfile()  {
 
@@ -14,11 +18,6 @@ const [fields, setFields] = useState([
 		},
 		{
 			name:'Surname',
-			text:'',
-			checked:'false'
-		},
-		{
-			name:'Nationality',
 			text:'',
 			checked:'false'
 		}
@@ -35,8 +34,16 @@ const [fields, setFields] = useState([
 			   ))
 			   }
 			   
-			   <br/>
-			   
+			    <FormControl style={{ minWidth: 120 }}>
+        		<InputLabel id="nationality-select">Naitonality</InputLabel>
+        		<Select labelId="nationality-select" >
+          		{CountryRegionData.map((option, index) => (
+           		 <MenuItem key={option[0]} value={option}>
+              		{option[0]}
+            		</MenuItem>
+        			  ))}
+        		</Select>
+      			</FormControl><br />
 			   <Button variant="contained"  component="label"	>
 			   	Attach VCF file   
 			   	<input     type="file"    style={{ display: "none" }}  />
