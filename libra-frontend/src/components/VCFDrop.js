417a33b690143594cd6efd5c99f23a327f2432e0
NaisilaPuka <nicypp@gmail.com>
2020-02-29 20:14:40 +0300

    Temporary VCF drop changes commented

--- libra-frontend/src/components/VCFDrop.js
+++ libra-frontend/src/components/VCFDrop.js
@@ -24,3 +24,52 @@ class VCFDrop extends Component {
 }
 
 export default VCFDrop;
+
+
+// export default function VCFDrop (){
+//     const [selectedFile, setSelectedFile] = React.useState()
+//     const [preview, setPreview] = React.useState()
+
+//     // create a preview as a side effect, whenever selected file is changed
+//     React.useEffect(() => {
+//         if (!selectedFile) {
+//             setPreview(undefined)
+//             return
+//         }
+
+//         const objectUrl = URL.createObjectURL(selectedFile)
+//         setPreview(objectUrl)
+//         console.log(objectUrl)
+
+//         // free memory when ever this component is unmounted
+//         // return () => URL.revokeObjectURL(objectUrl)
+//     }, [selectedFile])
+
+
+//     const onSelectFile = event => {
+//         if (!event.target.files || event.target.files.length === 0) {
+//             setSelectedFile(undefined)
+//             return
+//         }
+
+//         // I've kept this example simple by using the first image instead of multiple
+//         setSelectedFile(event.target.files[0])
+
+//         // console.log(event.target)
+//     }
+
+//     // return (
+//     //     <div>
+//     //         // <DropzoneArea
+//     //       // dropzoneText="Upload your VCF file(s)" acceptedFiles={[".vcf"]} showPreviewsInDropzone={true} showPreviews={false} showAlerts={true} useChipsForPreview={true} onChange={onSelectFile} />
+//     //       <input type="file" onChange={onSelectFile} />
+//     //       {selectedFile &&  <img src={preview} /> }
+//     //     </div>
+//     // )
+//     return (
+//         <div>
+//             <input type='file' onChange={onSelectFile} />
+//             {selectedFile &&  <img src={preview} /> }
+//         </div>
+//     )
+// }
