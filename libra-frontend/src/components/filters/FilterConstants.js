58846241c14b6189bf8e9f8b7ae4ef91fcf4cb5f
NaisilaPuka <nicypp@gmail.com>
2020-05-26 14:39:06 +0300

    Add scenario filter constants

--- libra-frontend/src/components/filters/FilterConstants.js
+++ libra-frontend/src/components/filters/FilterConstants.js
@@ -1,5 +1,5 @@
 export const defaultState = {
-    scenarioInput: "", 
+    scenarioInput: "none", 
     frequencyInput: {filterDbsnp: "any", filter1k: "any", filter1kfrequency: "1"}, 
     impactInput: {highImpactArray: [], medImpactArray: [], lowImpactArray: [], modifierImpactArray: []},
     pathogenicityInput: {polyphenArray: [], siftArray: [], polyphenScore: "0", siftScore: "1"}
